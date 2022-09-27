81d3dffd9119c3f2fe38785734caa4f3c3ba0896
Casper <casper.bjorkman@gmail.com>
2022-09-27 14:07:32 +0200

    Basic Parser Functions

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -1,1 +1,34 @@
+use std::iter::Peekable;
 
+use super::lexer::{Lexer, Token};
+
+pub struct Parser<'a> {
+    lexer: Peekable<Lexer<'a>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Self {
+            lexer: Lexer::new(src).peekable(),
+        }
+    }
+
+    pub fn peek(&mut self) -> Option<&Token> {
+        self.lexer.peek()
+    }
+
+    pub fn eat(&mut self) -> Option<Token> {
+        self.lexer.next()
+    }
+    pub fn eat_if<P: FnOnce(&Token) -> bool>(&mut self, p: P) -> Option<Token> {
+        if p(self.peek()?) {
+            self.eat()
+        } else {
+            None
+        }
+    }
+
+    pub fn is_at_eof(&mut self) -> bool {
+        self.lexer.peek().is_none()
+    }
+}
