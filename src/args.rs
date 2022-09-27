66036fc1bc7390489574bc0c279343b9a838ba97
Casper <casper.bjorkman@gmail.com>
2022-09-27 14:44:11 +0200

    basic file reading

--- src/args.rs
+++ src/args.rs
@@ -1,1 +1,7 @@
+use clap::Parser;
+use std::path::PathBuf;
 
+#[derive(Parser, Debug)]
+pub struct Args {
+    pub file: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::{fs::File, io::Read};
+
+use args::Args;
+use clap::Parser;
+
 mod args;
 mod frontend;
 
-fn main() {}
+fn main() {
+    let args = Args::parse();
+    let mut file = match File::open(args.file) {
+        Ok(a) => a,
+        Err(err) => panic!("{}", err),
+    };
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    println!("{}", text);
+}
