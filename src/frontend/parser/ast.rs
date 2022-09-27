4bfcd2dde7471fed81ed396181e5470372b8fe0c
Casper <casper.bjorkman@gmail.com>
2022-09-27 17:39:48 +0200

    Basic ast structure

---
+++ src/frontend/parser/ast.rs
@@ -0,0 +1,23 @@
+pub struct Module {
+    pub items: Vec<Item>,
+}
+
+pub enum Expr {
+    TODO,
+}
+
+pub struct IfBranch {
+    pub condition: Expr,
+    pub block: Block,
+}
+
+pub struct Block;
+pub struct IfConditonal {
+    pub if_branch: IfBranch,
+    pub else_if_branches: Vec<IfBranch>,
+    pub else_block: Option<Block>,
+}
+
+pub enum Item {
+    If(IfConditonal),
+}
