ac04b1d69804d8c1df78e37b5333dede2be8484c
Casper <casper.bjorkman@gmail.com>
2022-11-14 11:09:25 +0100

    added fn for resolving spans

--- src/utils/diagnostics.rs
+++ src/utils/diagnostics.rs
@@ -1,1 +1,20 @@
-struct DiagnosticContext<'a> {}
+use crate::frontend::span::Span;
+
+struct DiagnosticContext<'src> {
+    src: &'src str,
+    newlines: Vec<u32>,
+}
+pub fn resolve_span(newlines: &[u32], span: Span) -> (u32, u32) {
+    let index = span.byte_offset();
+    let newlines_before = newlines_before(newlines, index);
+    let newline_offset = newlines[newlines_before];
+    (index - newline_offset, newlines_before as u32)
+}
+
+fn newlines_before(newlines: &[u32], index: u32) -> usize {
+    let newlines_before = match newlines.binary_search(&index) {
+        Ok(num) | Err(num) => num,
+    };
+
+    newlines_before
+}
