b03f33ed7ae9bc67bbac676f59c1d1a6e8fd2500
Casper <casper.bjorkman@gmail.com>
2022-11-28 10:50:21 +0100

    added SrcFileCoordinate for document location

--- src/utils/diagnostics.rs
+++ src/utils/diagnostics.rs
@@ -4,18 +4,32 @@ struct DiagnosticContext<'src> {
     src: &'src str,
     newlines: Vec<u32>,
 }
-pub fn resolve_span(newlines: &[u32], span: Span) -> (u32, u32) {
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct SrcFileCoordinate {
+    pub line: u32,
+    pub column: u32,
+}
+
+pub fn resolve_span(newlines: &[u32], span: Span) -> SrcFileCoordinate {
     let index = span.byte_offset();
     let newlines_before = newlines_before(newlines, index);
     let newline_offset = newlines[newlines_before];
-    (index - newline_offset, newlines_before as u32)
+
+    SrcFileCoordinate {
+        line: newlines_before as u32,
+        column: index - newline_offset,
+    }
 }
-pub fn resolve_span_from_src(src: &str, span: Span) -> (u32, u32) {
+pub fn resolve_span_from_src(src: &str, span: Span) -> SrcFileCoordinate {
     let (before, after) = src.split_at(span.byte_offset() as usize);
     let col = before.len() - before.rfind('\n').unwrap_or_default();
-    let row = before.chars().filter(|&c| c == '\n').count();
+    let line = before.chars().filter(|&c| c == '\n').count();
 
-    (col as u32, row as u32)
+    SrcFileCoordinate {
+        line: line as u32,
+        column: col as u32,
+    }
 }
 
 fn newlines_before(newlines: &[u32], index: u32) -> usize {
