15a1ac1fc8dccad72fdbd7733ae1731cbb706e48
Casper <casper.bjorkman@gmail.com>
2022-11-29 14:41:48 +0100

    added io_adapter to enable testing of interpreter

--- src/evaluator/io_adaptor.rs
+++ src/evaluator/io_adaptor.rs
@@ -1,5 +1,49 @@
-use std::io::{self, Read, Stderr, Stdin, Stdout, Write};
+use std::{
+    collections::VecDeque,
+    io::{self, IoSlice, Read, Stderr, Stdin, Stdout, Write},
+    str::from_utf8,
+};
 
+struct ChunkWriter(Vec<Box<[u8]>>);
+
+pub struct TestingAdaptor {
+    in_queue: VecDeque<u8>,
+    out: ChunkWriter,
+    err: ChunkWriter,
+}
+
+impl Read for TestingAdaptor {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.in_queue.read(buf)
+    }
+}
+impl Write for TestingAdaptor {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.out.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        /*intentional blank */
+        Ok(())
+    }
+}
+impl Write for ChunkWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.push(buf.into());
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        /*intentional blank */
+        Ok(())
+    }
+}
+
+impl IoAdaptor for TestingAdaptor {
+    fn err(&mut self) -> &mut dyn Write {
+        &mut self.err
+    }
+}
 pub struct StdIOAdaptor {
     stdin: Stdin,
     stdout: Stdout,
@@ -21,7 +65,13 @@ impl Read for StdIOAdaptor {
 }
 impl Write for StdIOAdaptor {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.stdout.write(buf)
+        let yellow = b"\x1B[33m";
+        let reset = b"\x1B[0m";
+
+        // eprintln!("[IO ADAPTER] buf: {final_buf:?}");
+        self.stdout.write_all(&[yellow, buf, reset].concat())?;
+
+        Ok(buf.len())
     }
 
     fn flush(&mut self) -> io::Result<()> {
