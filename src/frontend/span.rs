ca6ee1bacb3531a7fa5c0601a1324c3264eeb828
Casper <casper.bjorkman@gmail.com>
2022-12-07 14:42:26 +0100

    expanded largest possible length of span. Also added convenience functions for combining spans

--- src/frontend/span.rs
+++ src/frontend/span.rs
@@ -3,18 +3,34 @@ use std::ops::Range;
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Span {
     byte_offset: u32,
-    len: u16,
+    len: u32,
 }
 impl Span {
-    pub(crate) fn byte_offset(&self) -> u32 {
+    pub fn byte_offset(&self) -> u32 {
         self.byte_offset
     }
-
-    pub fn len(&self) -> u16 {
+    
+    pub fn len(&self) -> u32 {
         self.len
     }
     pub fn into_src_range(self)-> Range<usize>{
         (self.byte_offset()as usize)..(self.byte_offset() as usize + self.len() as usize)
+    }
+    pub fn combine(self, rhs:Self) -> Self {
+        let start = Ord::min(self.byte_offset(), rhs.byte_offset());
+        let end = Ord::max(self.byte_offset() + self.len(), rhs.byte_offset() + rhs.len());
+
+        let len = end - start;
+
+
+        Span { byte_offset: start, len }
+    }
+    pub fn combine_mabye(self, rhs:Option<Self>) -> Self{
+        if let Some(rhs) = rhs {
+            self.combine(rhs)
+        } else {
+            self
+        }
     }
 }
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -39,4 +55,4 @@ impl TryFrom<Range<usize>> for Span {
             len,
         })
     }
-}
+}    
