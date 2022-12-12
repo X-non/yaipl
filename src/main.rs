e9576bd0c26a1414014963878b3e7c3f17ef9b75
Casper <casper.bjorkman@gmail.com>
2022-12-12 09:51:59 +0100

    startet parasing the cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,46 @@
+use clap::Parser as CLIParser;
+use logos::source;
+use std::fs::File;
+use std::io::{Read, Seek};
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
 
+use yaipl::cli::{self, CLIOptions, Command};
 use yaipl::evaluator;
 use yaipl::utils::diagnostics::{resolve_span_from_src, DiagnosticContext};
 
+use yaipl::frontend::parser::{self, Parser};
 use yaipl::utils::tree_pretty_printer::AstPrinter;
 use yaipl::{self, frontend::parser::ast::Ast};
 
-use yaipl::frontend::parser::{self, Parser};
-
 fn main() {
-    // let args = Args::parse();
-    // let mut file = match File::open(args.file) {
-    //     Ok(a) => a,
-    //     Err(err) => panic!("{}", err),
-    // };
-    // let mut text = String::new();
-    // file.read_to_string(&mut text).unwrap();
-    let text = r#"
-        fn main {
-            let a = "outer";
-            { 
-                let a = "inner";
-                print(a);
-            }
-            print(a);
-        }
-        "#;
+    let cli_options = CLIOptions::parse();
+    match &cli_options.command {
+        Command::Run { path } => run(&cli_options, path),
+    }
+}
 
-    let mut parser = Parser::new(text);
+fn run(options: &CLIOptions, path: &Path) {
+    let source = read_file(path);
+
+    let diagnostics = Rc::new(DiagnosticContext::new(&source));
+    let mut parser = Parser::new(&source);
     let parse_root_module = parser.parse_root_module();
     let (idents, strings) = parser.into_interners();
-    let diagnostics = Rc::new(DiagnosticContext::new(text));
-    println!("{:#?}", strings);
+
     match parse_root_module {
         Ok(module) => {
-            let ast = Ast::new(module, idents, strings);
+            let ast = Ast::new(module, idents, strings).annotate();
+
             println!(
                 "{}",
                 AstPrinter::from_node(
-                    &ast,
-                    ast.identifiers.clone(),
-                    ast.strings.clone(),
+                    &ast.ast,
+                    ast.ast.identifiers.clone(),
+                    ast.ast.strings.clone(),
                     diagnostics.clone()
                 )
             );
-            let ast = ast.annotate();
-            println!("{:#?}", ast.ast.identifiers);
-            println!("{:#?}", ast.ast.strings);
 
             println!("{:#?}", evaluator::evaluate(ast));
 
@@ -56,11 +50,19 @@ fn main() {
             parser::ParseError::UnexpectedToken(r) => {
                 eprintln!(
                     "UnexpectedToken: {:?} @ {:?}",
-                    &text[r.into_src_range()],
-                    resolve_span_from_src(text, r)
+                    &source[r.into_src_range()],
+                    resolve_span_from_src(&source, r)
                 )
             }
             rest => eprintln!("{rest:?}"),
         },
     }
+    drop(diagnostics);
+}
+
+fn read_file(path: &Path) -> String {
+    let mut file = File::open(path).expect("Could not find file");
+    let mut source = String::new();
+    file.read_to_string(&mut source).unwrap();
+    source
 }
