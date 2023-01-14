18d15f656d657017defa0ea0b0612d7cae5d38a5
Casper <casper.bjorkman@gmail.com>
2023-01-14 17:38:25 +0100

    Added an absolute FileCoordinate and renamed SrcFileCoordinate to LocalFileCoordinate.

--- src/utils/diagnostics.rs
+++ src/utils/diagnostics.rs
@@ -1,4 +1,8 @@
-use std::{cell::RefCell, path::Path, rc::Rc};
+use std::{
+    cell::RefCell,
+    path::{Path, PathBuf},
+    rc::Rc,
+};
 
 use crate::frontend::{parser::ParseError, span::Span};
 
@@ -19,7 +23,7 @@ impl<'src> DiagnosticContext<'src> {
             newlines: Default::default(),
         }
     }
-    pub fn resolve_span(&self, span: Span) -> SrcFileCoordinate {
+    pub fn resolve_span(&self, span: Span) -> LocalFileCoordinate {
         //FIXME: mabye use the cache
         resolve_span_from_src(self.src, span)
     }
@@ -42,35 +46,47 @@ impl<'src> DiagnosticContext<'src> {
     }
 }
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct FileCoordinate {
+    pub pos: LocalFileCoordinate,
+    pub file: Rc<Path>,
+}
+
+impl std::fmt::Display for FileCoordinate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.file.display(), self.pos)
+    }
+}
+
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-pub struct SrcFileCoordinate {
+pub struct LocalFileCoordinate {
     pub line: u32,
     pub column: u32,
 }
 
-impl std::fmt::Display for SrcFileCoordinate {
+impl std::fmt::Display for LocalFileCoordinate {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, ":{}:{}", self.line, self.column)
     }
 }
 
-pub fn resolve_span(newlines: &[u32], span: Span) -> SrcFileCoordinate {
+pub fn resolve_span(newlines: &[u32], span: Span) -> LocalFileCoordinate {
     let index = span.byte_offset();
     let newlines_before = newlines_before(newlines, index);
     let newline_offset = newlines[newlines_before];
 
-    SrcFileCoordinate {
+    LocalFileCoordinate {
         line: newlines_before as u32 + 1,
         column: index - newline_offset + 1,
     }
 }
-pub fn resolve_span_from_src(src: &str, span: Span) -> SrcFileCoordinate {
+pub fn resolve_span_from_src(src: &str, span: Span) -> LocalFileCoordinate {
     let before = &src[..(span.byte_offset() as usize)];
     let line = before.chars().filter(|&c| c == '\n').count() + 1; // line number starts counting at 1
     let fix_first_line = if line == 1 { 1 } else { 0 };
     let col = before.len() - before.rfind('\n').unwrap_or_default() + fix_first_line;
 
-    SrcFileCoordinate {
+    LocalFileCoordinate {
         line: line as u32,
         column: col as u32,
     }
